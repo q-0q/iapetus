51075f820366c8b9a8139c480f94dffcc52d2d32
q-0q <jrwthrs@gmail.com>
2025-11-18 11:00:08 -0500

    main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    private GameObject _main;
+    private GameObject _levelSelect;
+    private GameObject _settings;
+    private GameObject _credits;
+
+    private GameObject _current;
+
+    private GameObject _back;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _main = transform.Find("Main").gameObject;
+        _back = transform.Find("Back").gameObject;
+        _levelSelect = transform.Find("LevelSelect").gameObject;
+        // _settings = transform.Find("Settings").gameObject;
+        // _credits = transform.Find("Credits").gameObject;
+        
+        _levelSelect.SetActive(false);
+        _back.SetActive(false);
+    }
+
+    public void OnPlayClicked()
+    {
+        _main.SetActive(false);
+        _back.SetActive(true);
+        _levelSelect.SetActive(true);
+        _levelSelect.transform.Find("Buttons").Find("Tutorial").GetComponent<Button>().Select();
+    }
+    
+    public void OnSettingsClicked()
+    {
+        
+    }
+    
+    public void OnCreditsClicked()
+    {
+        
+    }
+
+    public void OnBackClicked()
+    {
+        _back.SetActive(false);
+        _levelSelect.SetActive(false);
+        _main.SetActive(true);
+        _main.transform.Find("Buttons").Find("Play").GetComponent<Button>().Select();
+    }
+
+    public void OnLevelSelected(string scene)
+    {
+        SceneLoader.Singleton.LoadScene(scene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
