e70f0459fedd2d88ddebbc0dd93787c65d6a977f
q-0q <jrwthrs@gmail.com>
2025-12-10 12:45:08 -0500

    cutscene singleton

--- Assets/CutsceneManager.cs
+++ Assets/CutsceneManager.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CutsceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static CutsceneManager Singleton;
+
+    private GameObject _activeCutscene;
+
+    private void Awake()
     {
-        
+        Singleton = this;
+        _activeCutscene = null;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetActiveCutscene(GameObject activeCutscene)
     {
-        
+        _activeCutscene = activeCutscene;
+    }
+
+    public void ClearActiveCutscene()
+    {
+        _activeCutscene = null;
+    }
+
+    public bool IsCutsceneActive()
+    {
+        return _activeCutscene != null;
     }
 }
