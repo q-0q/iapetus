851047366f27db5a5cb4672512fc773c57a4d97b
q-0q <jrwthrs@gmail.com>
2025-12-27 17:50:19 -0500

    prefab grid bake

--- Assets/Code/LevelComponents/PrefabGrid.cs
+++ Assets/Code/LevelComponents/PrefabGrid.cs
@@ -7,6 +7,8 @@ using UnityEngine.Serialization;
 public class PrefabGrid : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
+    [SerializeField] private float sizeX = 5f;
+    [FormerlySerializedAs("sizeY")] [SerializeField] private float sizeZ = 5f;
     [SerializeField] private float density = 1f;
     [SerializeField] private float maxPositionOffset = 1f;
     [SerializeField] private float minScale = 1f;
@@ -14,12 +16,25 @@ public class PrefabGrid : MonoBehaviour
     [SerializeField] private float minRotation = 0f;
     [SerializeField] private float maxRotation = 0f;
     
+    // Method with parameter that only works in editor mode.
+    [SerializeField, ButtonInvoke(nameof(Bake), 1.1f, ButtonInvoke.DisplayIn.EditMode)] private bool bake;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        var x = transform.lossyScale.x;
-        var z = transform.lossyScale.z;
+        // Bake();
+    }
+
+    private void Bake()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+        
+        var x = sizeX;
+        var z = sizeZ;
         for (float i = -x * 0.5f + transform.position.x; i < x * 0.5f + transform.position.x; i += density)
         {
             for (float j = -z * 0.5f + transform.position.z; j < z * 0.5f + transform.position.z; j += density)
@@ -28,10 +43,16 @@ public class PrefabGrid : MonoBehaviour
                 var obj = Instantiate(prefab, pos, Quaternion.identity);
                 obj.transform.rotation = Quaternion.Euler(0, UnityEngine.Random.Range(minRotation, maxRotation), 0f);
                 var s = UnityEngine.Random.Range(minScale, maxScale);
-                obj.transform.SetParent(transform.parent);
+                obj.transform.SetParent(transform);
                 obj.transform.localScale = new Vector3(s, s, s);
             }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(transform.position, new Vector3(sizeX, 1, sizeZ));
     }
 
     // Update is called once per frame
