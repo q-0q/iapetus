87b0fff1b09497663b1362561bde0a210f47cb60
q-0q <jrwthrs@gmail.com>
2026-01-05 18:22:19 -0500

    scene ambient particles

---
+++ Assets/SceneAmbientParticles.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneAmbientParticles : MonoBehaviour
+{
+    void Start()
+    {
+        transform.SetParent(PlayerFsm.Singleton.transform);
+    }
+    
+}
