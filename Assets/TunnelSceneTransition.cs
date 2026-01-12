51273f9e7a3748c7c699cfd80120c2ddd8adab37
q-0q <jrwthrs@gmail.com>
2026-01-11 21:03:35 -0500

    c1-2

--- Assets/TunnelSceneTransition.cs
+++ Assets/TunnelSceneTransition.cs
@@ -8,9 +8,30 @@ public class TunnelSceneTransition : MonoBehaviour
     public string scene;
     public Vector3 destinationPosition;
     public float destinationRotation;
-    
+    private float _initialFogEndDistance;
+    private float _initialFogStartDistance;
+
     private void OnTriggerEnter(Collider other)
     {
+        SaveSystem.WritePlayerInGamePosition(destinationPosition, destinationRotation, 0);
         SceneLoader.Singleton.LoadScene(scene);
     }
+
+    
+    // private void Start()
+    // {
+    //     _initialFogEndDistance = RenderSettings.fogEndDistance;
+    //     _initialFogStartDistance = RenderSettings.fogStartDistance;
+    // }
+    //
+    // private void Update()
+    // {
+    //     var distance = Vector3.Distance(transform.position, PlayerFsm.Singleton.transform.position);
+    //
+    //     var fogDistanceOffset = Mathf.Lerp(30f, 0f,
+    //         Mathf.InverseLerp(20f, 50f, distance));
+    //
+    //     RenderSettings.fogStartDistance = _initialFogStartDistance + fogDistanceOffset;
+    //     RenderSettings.fogEndDistance = _initialFogEndDistance + fogDistanceOffset;
+    // }
 }
