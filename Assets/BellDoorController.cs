e8830676b7bcbd2514dfb7282a7403430607ee78
q-0q <jrwthrs@gmail.com>
2026-02-28 11:43:00 -0500

    bell door

--- Assets/BellDoorController.cs
+++ Assets/BellDoorController.cs
@@ -13,11 +13,14 @@ public class BellDoorController : MonoBehaviour
     public static event Action OnPlayerNearbyUnopenedBellDoor;
     public string persistentEvent;
     private bool _opened;
+    private float _playerLookAwayTimer;
+    private const float LookawayTime = 0.75f;
 
     private void Awake()
     {
+        _playerLookAwayTimer = 0f;
         _animator = GetComponentInChildren<Animator>();
-        _passiveCamera = transform.Find("PassiveCamera").GetComponentInChildren<CinemachineVirtualCamera>();
+        _passiveCamera = transform.Find("BellDoorPassiveCamera").GetComponentInChildren<CinemachineVirtualCamera>();
     }
 
     // Start is called before the first frame update
@@ -32,13 +35,22 @@ public class BellDoorController : MonoBehaviour
         {
             Util.ReplaceAnimatorTrigger(_animator, "Open");
         }
+
+        if (Vector3.Angle(PlayerFsm.Singleton.transform.forward, transform.forward) < 90f)
+        {
+            _playerLookAwayTimer += Time.deltaTime;
+        }
+        else
+        {
+            _playerLookAwayTimer = 0;
+        }
         
         if (_passiveCamera.Priority > 0) OnPlayerNearbyUnopenedBellDoor?.Invoke();
     }
 
     private void OnTriggerStay(Collider other)
     {
-        _passiveCamera.Priority = 20;
+        _passiveCamera.Priority = _playerLookAwayTimer >= LookawayTime ? -20 : 20;
     }
 
     private void OnTriggerExit(Collider other)
